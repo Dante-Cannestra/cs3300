eb1a84f060f37b50e288cd5355412616dd3ef182
Dante Cannestra <dcannest@uccs.edu>
2022-11-26 01:30:39 +0000

    send help

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
     factory :user do
-      sequence(:email) { |n| "test#{n + 1}@email.com" }
+      #sequence(:email) { |n| "test#{n + 2}@email.com" }
+      email { 'test@email.com' }
       password { 'testPassword' }
       password_confirmation { 'testPassword' }
     end
